232d1754f6324d1e05d93f6e035df8f880d5f609
EYUP <110978352+EyupBas@users.noreply.github.com>
2023-02-06 21:54:29 +0300

    notOrtalamasi

---
+++ src/NotOrtalamasi.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class NotOrtalamasi {
+    public static void main(String[] args) {
+        int mat, fizik, kimya, muzik, turkce, tarih;
+        Scanner inp = new Scanner(System.in);
+        System.out.print("Matematik Notunuz :");
+        mat = inp.nextInt();
+
+        System.out.print("Fizik Notunuz : ");
+        fizik = inp.nextInt();
+
+        System.out.print("Kimya Notunuz : ");
+        kimya = inp.nextInt();
+
+        System.out.print("Müzik Notunuz : ");
+        muzik = inp.nextInt();
+
+        System.out.print("Türkçe Notunuz : ");
+        turkce = inp.nextInt();
+
+        System.out.print("Tarih Notunuz : ");
+        tarih = inp.nextInt();
+
+        double notOrt = (mat + kimya + fizik + muzik + tarih +turkce)/6;
+        System.out.println("Not Ortalamanız : " + notOrt);
+
+        boolean durum;
+        durum = (notOrt >= 60) ;
+        String vDurum = (durum) ? "Tebrikler Geçtiniz" : "Kaldiniz";
+        System.out.println(vDurum);
+
+    }
+
+}
