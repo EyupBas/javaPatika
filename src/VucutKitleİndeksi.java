df12f041c4595451d78fb6a2d365927ebca294ff
EYUP <110978352+EyupBas@users.noreply.github.com>
2023-02-07 21:17:26 +0300

    vucutKitleİndeks

---
+++ src/VucutKitleİndeksi.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class VucutKitleİndeksi {
+    public static void main(String[] args) {
+        /*
+        Vücut Kitle İndeksi Hesaplama
+Java ile kullanıcıdan boy ve kilo değerlerini alıp bir değişkene atayın.
+ Aşağıdaki formüle göre kullanıcının "Vücut Kitle İndeks" değerini hesaplayıp
+ ekrana yazdırın.
+
+Formül
+Kilo (kg) / Boy(m) * Boy(m)
+
+Çıktısı
+Lütfen boyunuzu (metre cinsinde) giriniz : 1,72
+Lütfen kilonuzu giriniz : 105
+Vücut Kitle İndeksiniz : 35.49215792320173
+         */
+
+        double kilo,boy;
+        Scanner input=new Scanner(System.in);
+        System.out.println("lütfen kilonuzu kg cinsinden giriniz");
+        kilo=input.nextDouble();
+
+        System.out.println("lütfen boyunuzu (metre cinsinden) giriniz");
+        boy=input.nextDouble();
+
+        System.out.println("vucut kitle indeksiniz :"+kilo/boy*boy);
+
+
+
+
+
+
+    }
+}
