9f008f7d336f9a32ed1bf63fd0a6fde3b0894d83
EYUP <110978352+EyupBas@users.noreply.github.com>
2023-02-09 17:08:52 +0300

    4dunKatiToplami

---
+++ src/GirilenTekSayiToplm.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class GirilenTekSayiToplm {
+    public static void main(String[] args) {
+        /*
+        Ödev
+Java döngüler ile tek bir sayı girilene kadar kullanıcıdan girişleri kabul eden ve
+ girilen değerlerden çift ve 4'ün katları olan sayıları toplayıp
+ ekrana basan programı yazıyoruz.
+         */
+        int sayi, sub = 0;
+        Scanner number = new Scanner(System.in);
+
+        do{
+            System.out.print("Lütfen bir sayı giriniz : ");
+            sayi = number.nextInt();
+            if(sayi % 4 == 0){
+                sub += sayi;
+            }
+        }while(sayi % 2 == 0);
+
+        System.out.println("4 ün katları olarak girilen sayıların toplamı : " + sub);
+    }
+    }
+
+
