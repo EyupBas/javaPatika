1a50ace38f76c68b04c9fa8a54eae817fc891c29
EYUP <110978352+EyupBas@users.noreply.github.com>
2023-02-09 20:20:01 +0300

    ustAlma

---
+++ src/UsluSayilar.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class UsluSayilar {
+    public static void main(String[] args) {
+        int n, u, total = 1;
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Üssünü almak istediginiz sayıyı giriniz : ");
+        n = input.nextInt();
+
+        System.out.print("Üs derecesini giriniz : ");
+        u = input.nextInt();
+
+        for (int i = 1; i <= u; i++){
+            total *= n;
+        }
+        System.out.println(n + " Saysının " + u + "." + " dereceden kuvveti : " + total);
+    }
+}
