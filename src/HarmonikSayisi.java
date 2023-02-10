38edc691ffe0e3349ab8ebe6ce9827bf34c00087
EYUP <110978352+EyupBas@users.noreply.github.com>
2023-02-10 15:55:43 +0300

    harmonikSayi

---
+++ src/HarmonikSayisi.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class HarmonikSayisi {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("n sayı giriniz : ");
+
+        int n = input.nextInt();
+        double result=0;
+        for (int i = 1; i <=n ; i++) {
+            result +=(1.0/i);
+
+        }
+        System.out.println(result);
+
+    }
+}
