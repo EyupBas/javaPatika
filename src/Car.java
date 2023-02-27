fa3c8fc3d84d405e1b8bcd99aa9d7553d08daf55
EYUP <110978352+EyupBas@users.noreply.github.com>
2023-02-27 13:15:43 +0300

    patika

---
+++ src/Car.java
@@ -0,0 +1,24 @@
+public class Car {
+    String type;
+    String model;
+    String color;
+    int speed;
+    int speedLimit=180;
+
+    void increaseSpeed(int increment) {
+
+        if((speed+increment)<speedLimit)
+        speed+=increment;
+
+    }
+    void decreaseSpeed(int decrease){
+        if(speed>0){
+            speed-=decrease;
+        }
+
+    }
+    void printSpeed(){
+        System.out.println("hızınız :"+speed);
+    }
+
+}
